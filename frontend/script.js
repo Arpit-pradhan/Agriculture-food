ce239e6cef264aeba93e267c674f1e259f73482c
prakash-saw <prakashsawsaw79@gmail.com>
2026-02-19 10:50:33 +0530

    folder added

---
+++ frontend/script.js
@@ -0,0 +1,8 @@
+//! hello hackers, this is the script file for the agriculture food website. you can add your JavaScript code here to enhance the functionality of the website. feel free to modify and customize it according to your needs. happy coding!
+
+// * Example: Add a click event listener to a button
+
+//  ? const myButton = document.getElementById('myButton');
+// todo myButton.addEventListener('click', function() {
+//  !   alert('Button clicked!');
+// * });
